cbb3bae9e8187ddab993adc04973459181880bec
Ming-der Wang <mingderwang@gmail.com>
2021-08-24 16:19:47 +0800

    release v0.0.1

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import ForgeUI, {
   Button,
   Text,
@@ -7,7 +9,6 @@ import ForgeUI, {
   IssuePanel,
   render,
 } from '@forge/ui'
-import { Sdk } from 'etherspot'
 
 import { fetch } from '@forge/api'
 
@@ -15,28 +16,49 @@ const sendLog = async () => Promise.resolve('send!')
 
 const LogData = ({ counter }) => {
   const [logSend, setLogSend] = useState()
+  const [trader, setTrader] = useState()
+  const [fast, setFast] = useState()
+  const [safeLow, setSafeLow] = useState()
+
+  const fetchGasInfo = async () => {
+    const result = await fetch('https://ethgasstation.info/api/ethgasAPI.json')
+    const data = await result.json();
+    const status = JSON.stringify(data)
+    setTrader(data.fastest/10)
+    setFast(data.fast/10)
+    setSafeLow(data.safeLow/10)
+    setLogSend(status)
+  }
 
   useEffect(async () => {
     await sendLog()
     setLogSend(Date.now())
-    const result = await fetch(
-      'https://ethgasstation.info/api/ethgasAPI.json',
-    )
-    const status = result.text()
-    setLogSend(status)
+    await fetchGasInfo()
   }, [counter])
 
-  return <Text>Last log: {logSend}</Text>
+  return (
+    <Fragment>
+      <Text>Trader (Fastest): {trader}</Text>
+      <Text>Fast (less than 2m): {fast}</Text>
+      <Text>Standard (less than 5m): {safeLow}</Text>
+      <Text>Last log: {logSend}</Text>
+    </Fragment>
+  )
 }
 
 const App = () => {
   const [count, setCount] = useState(0)
-  const [gas, setGas] = useState('unknown')
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(count + 1)
+    }, 5000)
+  }, [])
 
   return (
     <Fragment>
       <Button
-        text={`Refreash ${count}`}
+        text={`Refetch Data, ${count} ${count == 1 ? 'time' : 'times'}`}
         onClick={() => {
           setCount(count + 1)
         }}
